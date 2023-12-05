8863c434900984144cd09dad0a6636d542b14250
leomaresc <salazarleomar@gmail.com>
2023-12-04 21:00:37 -0300

    Casi listo

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
  
 const app = express();
 const port = 3000
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "postgres",
+    password: "123456",
+    port: "5432"
+});
+
+db.connect();
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,15 +23,32 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/login", async (req, res) => {
+    res.render("login.ejs")
+});
 
+app.post("/login", async (req, res) => {
+    
 });
 
 app.get("/register", async (req, res) => {
+    res.render("register.ejs")
+});
 
+app.post("/register", async (req, res) => {
+    const newUser = req.body.username;
+    const newPassword = req.body.password
+    try{
+        db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [newUser, newPassword]);
+        console.log("cuenta creada con exito");
+        res.redirect("/");
+    } catch(err) {
+        console.error(err)
+        res.redirect("/register");
+    }
 });
 
 app.get("/secrets", async (req, res) => {
-
+    res.render("secrets.ejs")
 });
 
 app.get("/submit", async (req, res) => {
